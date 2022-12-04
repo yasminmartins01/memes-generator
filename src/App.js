b2517a2f6cebc5a78599d69d2756f504a9b3b207
yasmin <yasmartins999@gmail.com>
2022-12-03 22:16:30 -0300

    finalmente lo logre

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import html2canvas from 'html2canvas';
 import './index.css';
 const App = () => {
 
@@ -19,7 +20,17 @@ const App = () => {
         setImagen(evento.target.value)
     }
 
-        return (
+    const onClickExportar = function(evento){
+
+      html2canvas(document.querySelector("#meme")).then(canvas => {
+        var img = canvas.toDataURL("image/png");
+        var link = document.createElement('a');
+        link.download = 'meme.png';
+        link.href = img;
+        link.click();
+      });
+    }
+      return (
             <div className='app'>
                 <div className='container'>
                     <h1 className='titulo'> Meme generator </h1>
@@ -36,14 +47,13 @@ const App = () => {
                     <input onChange={onChangeLinea1} type='text' placeholder='linea superior'></input>
                     <input onChange={onChangeLinea2} type='text' placeholder='linea inferior'></input>
 
-                    <button> Exportar </button>
-
-                    <span>{Linea1}</span>
-                    <span>{Linea2}</span>
+                    <button onClick={onClickExportar} className='button'> Descargar </button>
 
                 </div>
-                <div className='imagen'>
-                   <img src={"../../img/"+Image+".jpg"} alt="imagen" /> 
+                <div className='imagen' id="meme">
+                   <span>{Linea1}</span><br/>
+                   <span>{Linea2}</span>
+                   <img src={"/img/"+Imagen+".jpg"} alt="imagen" /> 
                 </div>
             
             </div>
